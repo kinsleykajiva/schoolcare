2e66c969ec96d5ba6b0cb68c4cd5948a0fcf2f21
kinsleykajiva <kajivakinsley@gmail.com>
2019-12-09 09:12:23 +0200

    employee details

--- view/staff/staff.php
+++ view/staff/staff.php
@@ -1,1 +1,107 @@
-stafffff 11111
+<div class="page-header card">
+    <div class="card-block">
+        <!--<h5 class="m-b-10"></h5>-->
+        <button onclick="openNewStaffDialog()" class="btn btn-round btn-mini btn-primary btn-sm"><i class="icofont icofont-user-alt-3"></i>New Staff</button>
+    </div>
+</div>
+
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <!-- Basic Form Inputs card start -->
+            <div class="card">
+                <div class="card-header">
+                    <h5>Personal Details</h5>
+
+                    <div class="card-header-right"><i class="icofont icofont-spinner-alt-5"></i></div>
+
+                    <div class="card-header-right">
+                        <i class="icofont icofont-spinner-alt-5"></i>
+                    </div>
+
+                </div>
+                <div class="card-block">
+
+                    <form>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Surname</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" placeholder="Type your title in Placeholder">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Predefine
+                                Input</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" value="Enter yout content after me">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Upload File</label>
+                            <div class="col-sm-10">
+                                <input type="file" class="form-control">
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                </div>
+            </div>
+            <!-- Basic Form Inputs card end -->
+
+            <!-- Input Alignment card start -->
+            <div class="card">
+                <div class="card-header">
+                    <h5>Contact Details</h5>
+
+                    <div class="card-header-right"><i class="icofont icofont-spinner-alt-5"></i></div>
+                </div>
+                <div class="card-block">
+                    <form onsubmit="return false;">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Phone No.</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control form-control-normal" placeholder="">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <button class="col-sm-2 btn btn-mini btn-round btn-success">New</button>
+                            <div class="col-sm-10" style="display: none">
+
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Address</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control form-control-uppercase" placeholder="">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <button class="col-sm-2 btn btn-mini btn-round btn-success">New</button>
+                            <div class="col-sm-10" style="display: none">
+
+                            </div>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+            <!-- Input Alignment card end -->
+        </div>
+    </div>
+</div>
