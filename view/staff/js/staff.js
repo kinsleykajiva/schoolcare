fd6430242a5ea517dc84759215403ad6473cb114
kinsleykajiva <kajivakinsley@gmail.com>
2019-12-09 13:23:58 +0200

    employee details

--- view/staff/js/staff.js
+++ view/staff/js/staff.js
@@ -1,8 +1,69 @@
 
 
-function openNewStaffDialog () {
-
+function closeNewStaffDialog () {
+		$("#div_card_Newstaff_details").slideUp('fast');
+	$("#btnCloseDialog").hide();
+	$("#btnNewDialog").show();
 }
+
+function openNewStaffDialog () {
+	$("#btnNewDialog").hide();
+	$("#btnCloseDialog").show();
+	$("#div_card_Newstaff_details").slideDown('fast');
+}
+
+function onNextToContactDetails(){
+	$("#div_card_personal_details").hide('slow',()=>{
+		$("#div_card_contact_details").show('slow');
+	});
+	
+}
+
+function onPrevFromFiles(){
+	
+	$("#div_card_personal_details").slideDown('slow');
+	$("#div_card_contact_details").slideUp('slow');
+}
+
+
+function onNextToFiles(){
+	$("#div_card_contact_details").slideUp('slow');
+	$("#div_card_files_details").slideDown('slow');
+}
+
+
+function onPrevToContact(){
+	
+	$("#div_card_files_details").slideUp('slow');
+	$("#div_card_contact_details").slideDown('slow');
+}
+
+function onsaveNewEmployee(){
+let name = $("#name").val();
+let surname = $("#surname").val();
+let id_num = $("#id_num").val();
+let sex = $("#sex").val();
+let date_of_birth = $("#date_of_birth").val();
+let pics  = $("#pics").val();
+let email = $("#email").val();
+let address = $("#address").val();
+let select_jobPosition = $("#select_jobPosition").val();
+let docss = $("#docss").val();
+
+let data_ = new FormData();
+	data_.append( name , name);
+	data_.append(surname , surname);
+	data_.append(id_num , id_num);
+	data_.append(sex ,sex );
+	data_.append(date_of_birth , date_of_birth);
+	data_.append( pics, pics.files[0]);
+	data_.append( email, email);
+	data_.append( address, address);
+	data_.append( select_jobPosition,select_jobPosition );
+	data_.append( docss,docss );
+}
+
+
 let phoneCounter = 1 ;
 function addPhoneOption () {
 	
--- view/staff/staff.php
+++ view/staff/staff.php
@@ -1,16 +1,17 @@
 <div class="page-header card">
     <div class="card-block">
         <!--<h5 class="m-b-10"></h5>-->
-        <button onclick="openNewStaffDialog()" class="btn btn-round btn-mini btn-primary btn-sm"><i class="icofont icofont-user-alt-3"></i>New Staff</button>
+        <button id="btnNewDialog" onclick="openNewStaffDialog()" class="btn btn-round btn-mini btn-primary btn-sm"><i class="icofont icofont-user-alt-3"></i>New Staff</button>
+        <button id="btnCloseDialog" style="display: none;" onclick="closeNewStaffDialog()" class="btn btn-round btn-mini btn-warning btn-sm"><i class="icofont icofont-close-alt-3"></i>Cancel</button>
     </div>
 </div>
 
 
-<div class="page-body">
+<div class="page-body" id="div_card_Newstaff_details" style="display: none;">
     <div class="row">
         <div class="col-sm-12">
             <!-- Basic Form Inputs card start -->
-            <div class="card">
+            <div class="card" id="div_card_personal_details" >
                 <div class="card-header">
                     <h5>Personal Details</h5>
 
@@ -23,34 +24,59 @@
                 </div>
                 <div class="card-block">
 
-                    <form>
+                    <form onsubmit="return false;">
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">Name</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control">
+                                <input type="text" id="name"  class="form-control">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">Surname</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" placeholder="Type your title in Placeholder">
+                                <input type="text" id="surname" class="form-control" placeholder="">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">ID Num.</label>
+                            <div class="col-sm-10">
+                                <input type="text" id="id_num"  class="form-control" placeholder="">
                             </div>
                         </div>
 
 
                         <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Predefine
-                                Input</label>
+                            <label class="col-sm-2 col-form-label">Sex</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" value="Enter yout content after me">
+                                <select type="text" id="sex" class="form-control" >
+                                    <option value="null">Select</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Date Of Birth</label>
+                            <div class="col-sm-10">
+                                <input type="text" id="date_of_birth"  class="form-control" value="Enter yout content after me">
                             </div>
                         </div>
 
 
                         <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Upload File</label>
+                            <label class="col-sm-2 col-form-label">Upload Picture</label>
                             <div class="col-sm-10">
-                                <input type="file" class="form-control">
+                                <input type="file" id="pics" accept="image/png"  class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+
+                            <button onclick="onNextToContactDetails()" class="col-sm-2 btn btn-mini btn-round btn-info">Next </button>
+                            <div class="col-sm-10" style="display: none">
+
                             </div>
                         </div>
 
@@ -62,7 +88,7 @@
             <!-- Basic Form Inputs card end -->
 
             <!-- Input Alignment card start -->
-            <div class="card">
+            <div class="card" id="div_card_contact_details" style="display: none;">
                 <div class="card-header">
                     <h5>Contact Details</h5>
 
@@ -70,42 +96,75 @@
                 </div>
                 <div class="card-block">
                     <form onsubmit="return false;">
-                        <div id="phoneNumberDiv"></div>
-                        <!--<div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Phone No.</label>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Email Address</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control form-control-normal" placeholder="">
+                                <input type="email" id="email" class="form-control form-control-lowercase" placeholder="">
                             </div>
 
-                            <div class="col-sm-2">
-                                <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="optionsRadios" id="default_" value="1">
-                                   Default
-                                </label>
-                            </div>
-                        </div>-->
+                        </div>
+                        <div id="phoneNumberDiv"></div>
 
                         <div class="form-group row">
-                            <button class="col-sm-2 btn btn-mini btn-round btn-success">New</button>
+                            <button onclick="addPhoneOption ();" class="col-sm-2 btn btn-mini btn-round btn-success">Add Number</button>
                             <div class="col-sm-10" style="display: none">
 
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label class="col-sm-2 col-form-label">Address</label>
+                            <label class="col-sm-2 col-form-label">Address  </label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control form-control-uppercase" placeholder="">
+                                <input type="text" id="address" class="form-control form-control-capitalize" placeholder="(comma separate)">
                             </div>
-                            <div class="col-sm-2">
-                                <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="optionsRadios" id="default_" value="1">
-                                    Default
-                                </label>
+
+                        </div>
+
+                        <div class="form-group row" style="margin-top: 20px;">
+                            <button onclick="onPrevFromFiles()" class="col-sm-2 btn btn-mini btn-round btn-default">Prev </button>
+                            <button style="visibility: hidden;" class="col-sm-2 btn btn-mini btn-round btn-info">. </button>
+                            <button onclick="onNextToFiles()" class="col-sm-2 btn btn-mini btn-round btn-info">Next </button>
+                            <div class="col-sm-10" style="display: none">
+
                             </div>
                         </div>
 
+
+                    </form>
+                </div>
+            </div>
+            <!-- Input Alignment card end -->
+
+            <!-- Input Alignment card start -->
+            <div class="card" id="div_card_files_details" style="display: none;">
+                <div class="card-header">
+                    <h5>Files Details</h5>
+
+                    <div class="card-header-right"><i class="icofont icofont-spinner-alt-5"></i></div>
+                </div>
+                <div class="card-block">
+                    <form onsubmit="return false;">
+
                         <div class="form-group row">
-                            <button class="col-sm-2 btn btn-mini btn-round btn-success">New</button>
+                            <label class="col-sm-2 col-form-label">Position</label>
+                            <div class="col-sm-8">
+                                <select type="text" id="select_jobPosition" class="form-control ">
+                                    <option value="null">Select Position</option>
+                                </select>
+                            </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Documents</label>
+                            <div class="col-sm-8">
+                                <input type="file" multiple id="docss" accept="text/plain,application/pdf,image/png," class="form-control form-control-uppercase" placeholder="">
+                            </div>
+
+                        </div>
+
+                        <div class="form-group row" style="margin-top: 20px;">
+                            <button onclick="onPrevToContact()" class="col-sm-2 btn btn-mini btn-round btn-default">Prev </button>
+                            <button style="visibility: hidden;" class="col-sm-2 btn btn-mini btn-round btn-info">. </button>
+                            <button onclick="onsaveNewEmployee()" class="col-sm-2 btn btn-mini btn-round btn-info">Save </button>
                             <div class="col-sm-10" style="display: none">
 
                             </div>
