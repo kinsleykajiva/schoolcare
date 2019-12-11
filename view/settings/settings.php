edb57cf3ec55d3c993ad60e11b66ffa08ead80d7
kinsleykajiva <kajivakinsley@gmail.com>
2019-12-11 10:13:55 +0200

    done with Employee Access

--- view/settings/settings.php
+++ view/settings/settings.php
@@ -1,1 +1,68 @@
-settingssss 11111
+<div class="page-body">
+	<div class="row">
+		<div class="col-sm-12">
+			<!-- Material tab card start -->
+			<div class="card">
+				<div class="card-header">
+
+					<div class="card-header-right">
+						<ul class="list-unstyled card-option">
+							<li><i class="fa fa-chevron-left"></i></li>
+							<li><i class="fa fa-window-maximize full-card"></i></li>
+							<li><i class="fa fa-minus minimize-card"></i></li>
+							<li><i class="fa fa-refresh reload-card"></i></li>
+
+						</ul>
+					</div>
+
+				</div>
+				<div class="card-block">
+					<!-- Row start -->
+					<div class="row m-b-30">
+						<div class="col-lg-12 col-xl-12">
+							<div class="sub-title">Settings</div>
+							<!-- Nav tabs -->
+							<ul class="nav nav-tabs md-tabs" role="tablist">
+								<li class="nav-item">
+									<a class="nav-link active" data-toggle="tab" href="#home3" role="tab" aria-expanded="true">Users</a>
+									<div class="slide"></div>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" data-toggle="tab" href="#profile3" role="tab" aria-expanded="false">Profile</a>
+									<div class="slide"></div>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" data-toggle="tab" href="#messages3" role="tab" aria-expanded="false">Messages</a>
+									<div class="slide"></div>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" data-toggle="tab" href="#settings3" role="tab" aria-expanded="false">Settings</a>
+									<div class="slide"></div>
+								</li>
+							</ul>
+							<!-- Tab panes -->
+							<div class="tab-content card-block">
+								<div class="tab-pane active" id="home3" role="tabpanel" aria-expanded="true">
+									<p class="m-0">1. This is Photoshop's version of Lorem IpThis is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean mas Cum sociis natoque penatibus et magnis dis.....</p>
+								</div>
+								<div class="tab-pane" id="profile3" role="tabpanel" aria-expanded="false">
+									<p class="m-0">2.Cras consequat in enim ut efficitur. Nulla posuere elit quis auctor interdum praesent sit amet nulla vel enim amet. Donec convallis tellus neque, et imperdiet felis amet.</p>
+								</div>
+								<div class="tab-pane" id="messages3" role="tabpanel" aria-expanded="false">
+									<p class="m-0">3. This is Photoshop's version of Lorem IpThis is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean mas Cum sociis natoque penatibus et magnis dis.....</p>
+								</div>
+								<div class="tab-pane" id="settings3" role="tabpanel" aria-expanded="false">
+									<p class="m-0">4.Cras consequat in enim ut efficitur. Nulla posuere elit quis auctor interdum praesent sit amet nulla vel enim amet. Donec convallis tellus neque, et imperdiet felis amet.</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<!-- Row end -->
+
+				</div>
+			</div>
+			<!-- Material tab card end -->
+		</div>
+	</div>
+</div>
