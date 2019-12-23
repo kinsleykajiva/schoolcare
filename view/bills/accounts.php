17321ad606e5a5422f423a2f711f3c01a59f1750
kinsleykajiva <kajivakinsley@gmail.com>
2019-12-23 22:01:33 +0200

    working on accounting

--- view/bills/accounts.php
+++ view/bills/accounts.php
@@ -1,1 +1,51 @@
-<?php
+<div class="page-header card">
+	<div class="card-block">
+
+		<ul class="nav nav-tabs md-tabs" role="tablist">
+			<li class="nav-item">
+				<a class="nav-link " data-toggle="tab" href="#home3" role="tab">Children</a>
+				<div class="slide"></div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link active" data-toggle="tab" href="#profile3" role="tab">Fees</a>
+				<div class="slide"></div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#messages3" role="tab">Invoices</a>
+				<div class="slide"></div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#settings3" role="tab">Settings</a>
+				<div class="slide"></div>
+			</li>
+		</ul>
+
+	</div>
+</div>
+
+<div class="card">
+	<div class="tab-content card-block">
+		<div class="tab-pane " id="home3" role="tabpanel">
+			<p class="m-0">
+				1.
+				This is Photoshop's version of Lorem IpThis is Photoshop's version of Lorem Ipsum.
+
+			</p>
+		</div>
+		<div class="tab-pane active" id="profile3" role="tabpanel">
+			<p class="m-0">
+				<?php include 'fees/fees.php';?>
+
+			</p>
+		</div>
+		<div class="tab-pane" id="messages3" role="tabpanel">
+			<p class="m-0">3. This is Photoshop's version of Lorem ollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum,
+
+
+			</p>
+		</div>
+		<div class="tab-pane" id="settings3" role="tabpanel">
+			<p class="m-0">4.Cras elis amet.</p>
+		</div>
+	</div>
+</div>
---
+++ view/bills/fees/fees.php
@@ -0,0 +1,40 @@
+<div class="row">
+	<div class="col-lg-12 col-xl-12">
+
+		<!-- Nav tabs -->
+		<ul class="nav nav-tabs md-tabs tabs-left b-none" role="tablist">
+			<li class="nav-item">
+				<a class="nav-link active" data-toggle="tab" href="#home5" role="tab" aria-expanded="true">Fees Lists</a>
+				<div class="slide"></div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#profile5" role="tab" aria-expanded="false">Fees Structures</a>
+				<div class="slide"></div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#messages5" role="tab" aria-expanded="false">Messages</a>
+				<div class="slide"></div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#settings5" role="tab" aria-expanded="false">Settings</a>
+				<div class="slide"></div>
+			</li>
+		</ul>
+		<!-- Tab panes -->
+		<div class="tab-content tabs-left-content card-block">
+			<div class="tab-pane active" id="home5" role="tabpanel" aria-expanded="true">
+				<p class="m-0">1. This is Photoshop's version of Lorem IpThis is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean mas Cum sociis natoque penatibus et magnis dis.....</p>
+			</div>
+			<div class="tab-pane" id="profile5" role="tabpanel" aria-expanded="false">
+				<p class="m-0">2.Cras consequat in enim ut efficitur. Nulla posuere elit quis auctor interdum praesent sit amet nulla vel enim amet. Donec convallis tellus neque, et imperdiet felis amet.</p>
+			</div>
+			<div class="tab-pane" id="messages5" role="tabpanel" aria-expanded="false">
+				<p class="m-0">3. This is Photoshop's version of Lorem IpThis is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean mas Cum sociis natoque penatibus et magnis dis.....</p>
+			</div>
+			<div class="tab-pane" id="settings5" role="tabpanel" aria-expanded="false">
+				<p class="m-0">4.Cras consequat in enim ut efficitur. Nulla posuere elit quis auctor interdum praesent sit amet nulla vel enim amet. Donec convallis tellus neque, et imperdiet felis amet.</p>
+			</div>
+		</div>
+	</div>
+
+</div>
